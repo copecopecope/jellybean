f66e672b2ba0240f5f1ad19954f5f73b3f31bdcb
copecopecope <alexjcope@gmail.com>
2014-03-13 02:53:38 -0700

    Nothing down that rabbit hole know what I'm sayin

--- segmentation/dispclusters.m
+++ segmentation/dispclusters.m
@@ -1,7 +1,15 @@
-function dispclusters( count, clusters )
-%DISPCLUSTERS Summary of this function goes here
-%   Detailed explanation goes here
+function [img] = dispclusters( count, clusters )
 
+[m,n] = size(clusters);
+cmap = hsv(count);
+cmap = cmap(randperm(count),:);
+
+img = zeros(m,n,3);
+for i=1:m
+    for j=1:n
+        img(i,j,:) = reshape(cmap(clusters(i,j),:),1,1,3);
+    end
+end
 
 end
 
